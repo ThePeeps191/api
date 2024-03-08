d3764f101f2311837625e33d2e1647f6ae6c41b8
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2024-03-07 21:29:22 -0700

    base1 images

---
+++ download_images.py
@@ -0,0 +1,21 @@
+import json, urllib.request
+
+setid = "base1"
+apiurl = "https://thepeeps191.github.io/api/"
+
+with open(f"cards/{setid}.json") as f:
+	x = json.load(f)
+
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+urllib.request.install_opener(opener)
+
+for i in x:
+	print(i["name"])
+	urllib.request.urlretrieve(i["images"]["small"], i["images"]["small"].replace("pt", "").replace("https://images.pokemontcg.io/", ""))
+	urllib.request.urlretrieve(i["images"]["large"], i["images"]["large"].replace("pt", "").replace("https://images.pokemontcg.io/", ""))
+	i["images"]["small"] = apiurl + i["images"]["small"].replace("pt", "").replace("https://images.pokemontcg.io/", "")
+	i["images"]["large"] = apiurl + i["images"]["large"].replace("pt", "").replace("https://images.pokemontcg.io/", "")
+
+with open(f"cards/{setid}.json", "w") as f:
+	json.dump(x, f, indent = 4)
